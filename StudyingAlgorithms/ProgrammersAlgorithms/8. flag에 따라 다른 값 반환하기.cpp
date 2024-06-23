9a59a7ab6a3d75a4b60a57599669b2422b7bd6b8
naverstarcraft <starcraft0810@naver.com>
2024-06-23 22:22:52 +0900

    flag에 따라 다른 값 반환하기

---
+++ StudyingAlgorithms/ProgrammersAlgorithms/8. flag에 따라 다른 값 반환하기.cpp
@@ -0,0 +1,33 @@
+#include "../header/pch.h"
+
+#if TypesOfAlgorithmSites == 0
+#if AlgorithmNumber == 8
+
+#if QuestionNumber == 1
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, bool flag) {
+    return flag ? (a + b) : (a - b);
+}
+
+
+int main()
+{
+    cout << solution(-4, 7, true) << '\n';
+    cout << solution(-4, 7, false);
+
+
+    return 0;
+}
+
+#endif
+
+#if QuestionNumber == 2
+#endif
+#endif
+#endif
+
+
--- StudyingAlgorithms/header/pch.h
+++ StudyingAlgorithms/header/pch.h
@@ -2,8 +2,8 @@
 //#include "../bits/stdc++.h"
 
 #define TypesOfAlgorithmSites 0
-#define AlgorithmNumber 7
-#define QuestionNumber 2
+#define AlgorithmNumber 8
+#define QuestionNumber 1
 
 #include <iostream>
 
