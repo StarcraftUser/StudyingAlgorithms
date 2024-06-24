57f9986bebc94f22e1de4d48ea2081aa2d0826ec
naverstarcraft <starcraft0810@naver.com>
2024-06-24 16:27:27 +0900

    홀짝에 따라 다른 값 반환하기

---
+++ StudyingAlgorithms/ProgrammersAlgorithms/10. 홀짝에 따라 다른 값 반환하기.cpp
@@ -0,0 +1,46 @@
+#include "../header/pch.h"
+
+#if TypesOfAlgorithmSites == 0
+#if AlgorithmNumber == 10
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#if QuestionNumber == 1
+
+int solution(int n)
+{
+    int answer = 0;
+	for (int i = 1; i <= n; i++)
+	{
+        if (n % 2 == 0)
+        {
+            if (i % 2 == 0)
+                answer += i * i;
+        }
+        else
+        {
+            if (i % 2 == 1)
+                answer += i;
+        }
+	}
+
+    return answer;
+}
+
+int main()
+{
+    cout << solution(7) << '\n' << solution(10) << '\n';
+    return 0;
+}
+
+#endif
+
+#if QuestionNumber == 2
+
+#endif
+#endif
+#endif
+
+
--- StudyingAlgorithms/header/pch.h
+++ StudyingAlgorithms/header/pch.h
@@ -2,8 +2,8 @@
 //#include "../bits/stdc++.h"
 
 #define TypesOfAlgorithmSites 0
-#define AlgorithmNumber 1
-#define QuestionNumber 2
+#define AlgorithmNumber 10
+#define QuestionNumber 1
 
 #include <iostream>
 
