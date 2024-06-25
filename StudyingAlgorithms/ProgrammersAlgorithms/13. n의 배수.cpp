591ebc9de438cc96e703b584b1437fe742449d59
naverstarcraft <starcraft0810@naver.com>
2024-06-25 23:12:56 +0900

    n의 배수

---
+++ StudyingAlgorithms/ProgrammersAlgorithms/13. n의 배수.cpp
@@ -0,0 +1,34 @@
+#include "../header/pch.h"
+
+#if TypesOfAlgorithmSites == 0
+#if AlgorithmNumber == 13
+#if QuestionNumber == 1
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num, int n) {
+    return num % n == 0;
+}
+int main()
+{
+    cout << solution(98, 2) << '\n';
+    cout << solution(34, 3) << '\n';
+    return 0;
+}
+
+#endif
+
+#if QuestionNumber == 2
+
+int main()
+{
+    return 0;
+}
+#endif
+#endif
+#endif
+
+
