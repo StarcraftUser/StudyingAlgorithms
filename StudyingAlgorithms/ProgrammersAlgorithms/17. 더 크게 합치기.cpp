12bba41dc9de5351ec886037dedabdc0f759e8b8
naverstarcraft <starcraft0810@naver.com>
2024-06-29 02:03:54 +0900

    더 크게 합치기

---
+++ StudyingAlgorithms/ProgrammersAlgorithms/17. 더 크게 합치기.cpp
@@ -0,0 +1,31 @@
+#include "../header/pch.h"
+
+#if TypesOfAlgorithmSites == 0
+#if AlgorithmNumber == 17
+#if QuestionNumber == 1
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    string tempString = to_string(a) + to_string(b);
+    int tempNum1 = stoi(tempString);
+    tempString = to_string(b) + to_string(a);
+    int tempNum2 = stoi(tempString);
+    return tempNum1 >= tempNum2 ? tempNum1 : tempNum2;
+}
+int main()
+{
+    cout << solution(9, 91) << '\n';
+    cout << solution(89, 8) << '\n';
+    return 0;
+}
+
+#endif
+
+#if QuestionNumber == 2
+
+#endif
+#endif
+#endif
--- StudyingAlgorithms/header/pch.h
+++ StudyingAlgorithms/header/pch.h
@@ -2,7 +2,7 @@
 //#include "../bits/stdc++.h"
 
 #define TypesOfAlgorithmSites 0
-#define AlgorithmNumber 16
+#define AlgorithmNumber 17
 #define QuestionNumber 1
 
 #include <iostream>
