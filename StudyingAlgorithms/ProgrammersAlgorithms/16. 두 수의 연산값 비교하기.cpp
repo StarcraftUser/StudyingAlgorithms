0c5a48367cd9dbb16fa322610408738c5d7981fb
naverstarcraft <starcraft0810@naver.com>
2024-06-29 01:52:37 +0900

    두 수의 연산값 비교하기

---
+++ StudyingAlgorithms/ProgrammersAlgorithms/16. 두 수의 연산값 비교하기.cpp
@@ -0,0 +1,32 @@
+#include "../header/pch.h"
+
+#if TypesOfAlgorithmSites == 0
+#if AlgorithmNumber == 16
+#if QuestionNumber == 1
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b) {
+    int numAB = stoi(to_string(a) + to_string(b));
+    int temp = 2 * a * b;
+    return numAB > temp ? numAB : temp;
+}
+
+int main()
+{
+    cout << solution(2, 91) << '\n';
+    cout << solution(91, 2) << '\n';
+    return 0;
+}
+
+#endif
+
+#if QuestionNumber == 2
+
+#endif
+#endif
+#endif
+
+
