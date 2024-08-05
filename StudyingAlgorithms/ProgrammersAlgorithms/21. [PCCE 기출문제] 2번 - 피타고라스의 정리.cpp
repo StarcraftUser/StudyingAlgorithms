dbdcac4446bfb28ac163fa2e0babb10b5f9fb056
naverstarcraft <starcraft0810@naver.com>
2024-08-05 16:34:22 +0900

    21. [PCCE 기출문제] 2번 - 피타고라스의 정리

---
+++ StudyingAlgorithms/ProgrammersAlgorithms/21. [PCCE 기출문제] 2번 - 피타고라스의 정리.cpp
@@ -0,0 +1,23 @@
+#include "../header/pch.h"
+
+#if TypesOfAlgorithmSites == 0
+#if AlgorithmNumber == 21
+#if QuestionNumber == 1
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int a;
+    int c;
+    cin >> a >> c;
+    int b_square = c * c - a * a;
+    cout << b_square << endl;
+    return 0;
+}
+#endif
+
+#if QuestionNumber == 2
+
+#endif
+#endif
+#endif
--- StudyingAlgorithms/header/pch.h
+++ StudyingAlgorithms/header/pch.h
@@ -2,7 +2,7 @@
 //#include "../bits/stdc++.h"
 
 #define TypesOfAlgorithmSites 0
-#define AlgorithmNumber 20
+#define AlgorithmNumber 21
 #define QuestionNumber 1
 
 #include <iostream>
